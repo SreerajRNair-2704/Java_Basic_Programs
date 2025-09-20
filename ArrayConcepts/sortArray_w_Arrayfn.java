c441b9cb66b34cd422b5332ee2d5a0e3c76cbdf8
SreerajRNair-2704 <ygmesreeru@gmail.com>
2025-09-20 12:02:56 +0530

    Create sortArray_w_Arrayfn.java
    Sort Array with Array function

---
+++ ArrayConcepts/sortArray_w_Arrayfn.java
@@ -0,0 +1,19 @@
+/*
+
+Write a program to sort the below array using Array functions.
+
+array = {2,6,1,4,9}
+
+*/
+package ArrayConcept;
+
+import java.util.Arrays;
+public class sortArray_w_Arrayfn{
+  public static void main(String[] args){
+    int arr[] = {2,6,1,4,9};
+    Arrays.sort(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+}
+
+  
