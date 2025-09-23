e11b90aedb00ee97bdb1cd06ca344938cca90cc5
SreerajRNair-2704 <ygmesreeru@gmail.com>
2025-09-23 19:01:24 +0530

    helloWorld.java

--- BasicPrograms/helloWorld.java
+++ BasicPrograms/helloWorld.java
@@ -1,13 +1,10 @@
 package BasicPrograms;
 
-public class exampleofMethods {
-	public void sampleMethod() {
-		System.out.println("This is written inside a sample method.");
-	}
+public class helloWorld {
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		exampleofMethods variable1 = new exampleofMethods();
-		variable1.sampleMethod();
+		System.out.println("Hello World !");
+
 	}
 
 }
